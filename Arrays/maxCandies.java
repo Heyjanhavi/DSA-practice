215a4b35f9a2889111a646568b100e39d8ffe77e
Heyjanhavi <159689153+Heyjanhavi@users.noreply.github.com>
2026-01-23 19:30:08 +0530

    Add solution for kids with extra candies problem
    Implement solution to determine which kids can have the greatest number of candies after receiving extra candies.

---
+++ Arrays/maxCandies.java
@@ -0,0 +1,26 @@
+/* Problem: There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+Note that multiple kids can have the greatest number of candies */
+
+import java.util.Scanner;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List <Boolean> result = new ArrayList<>();
+        int max = 0;
+        
+        for (int c: candies){
+            max = Math.max(max,c);
+        }
+
+        for (int c:candies){
+            if (c + extraCandies >= max){
+                result.add(true);
+            }else {
+                result.add(false);
+            }
+        }
+        return result;
+        
+    }
+}
