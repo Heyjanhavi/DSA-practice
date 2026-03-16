0f37e1b9dcdeb7cace66e8e4a49af81ae290fc6b
Heyjanhavi <159689153+Heyjanhavi@users.noreply.github.com>
2026-03-16 19:58:26 +0530

    Add ValidParenthesis class to check parentheses validity
    Implement a method to check the validity of parentheses using a stack.

---
+++ Strings/ValidParenthesis.java
@@ -0,0 +1,41 @@
+/* Problem: check if the parenthesis are valid*/
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean isValid(String s){
+         Stack<Character> stack = new Stack<>();
+         for (int i=0; i<s.length(); i++){
+             char c = s.charAt(i);
+             if (c == '(' || c == '[' || c == '{'){
+                 stack.push(c);
+             }else {
+                 if (stack.isEmpty()){
+                     return false;
+                 }
+                 char top = stack.pop();
+                 if ((c == ')' && top != '(') ||
+                         (c == ']' && top != '[') ||
+                         (c == '}' && top != '{')){
+                     return false;
+                 }
+             }
+
+         }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string of parenthesis");
+        String s = sc.nextLine();
+        if (isValid(s)){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+
+
+    }
+}
