e2caaf7363276bf51a37b95507099f10d5b97f4b
Heyjanhavi <159689153+Heyjanhavi@users.noreply.github.com>
2026-04-02 22:29:18 +0530

    Add SetMismatch solution to find error numbers
    Implement solution to find duplicate and missing numbers in an array.

---
+++ Arrays/SetMismatch.java
@@ -0,0 +1,29 @@
+/* Problem: You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+You are given an integer array nums representing the data status of this set after the error.
+Find the number that occurs twice and the number that is missing and return them in the form of an array. */
+
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n+1];
+
+        for (int num : nums){
+            freq[num]++;
+        }
+        int duplicate = -1;
+        int missing = -1;
+        for (int i=1; i<=n; i++){
+            if (freq[i] == 2){
+                duplicate = i;
+            }
+            if (freq[i] == 0){
+                missing = i;
+            }
+
+        }
+        return new int[]{duplicate, missing};
+    }
+}
+
+ 
