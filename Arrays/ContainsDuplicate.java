295eb178ce9aebf0bb2fb103af6d52785234cbb7
Heyjanhavi <159689153+Heyjanhavi@users.noreply.github.com>
2026-09-01 15:01:25 +0530

    Add containsDuplicate method to check for duplicates
    Implemented a method to check for duplicate elements in an array using a HashSet.

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,30 @@
+/* Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3 */
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i=0; i<nums.length; i++){
+            if (set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+            
+        }
+        return false;
+        
+    }
+}
